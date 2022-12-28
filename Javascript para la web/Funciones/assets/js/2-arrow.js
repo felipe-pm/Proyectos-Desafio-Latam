504f5fe8ebc53ec1d83583f1d2d5b95137aaf336
felipe-pm <pipeeen@gmail.com>
2022-12-28 18:41:16 -0300

    Se agrega el desafio 3 Funciones

---
+++ Javascript para la web/Funciones/assets/js/2-arrow.js
@@ -0,0 +1,5 @@
+const suma = function(a, b){
+    return a+b
+}
+
+const suma1 = (a, b) => a+b
---
+++ Javascript para la web/Funciones/assets/js/script.js
@@ -0,0 +1,6 @@
+element = document.getElementById("ele1")
+
+function pintar(color = "green") {
+    element.style.backgroundColor = color
+}
+element.addEventListener("click", () => pintar("yellow"));
